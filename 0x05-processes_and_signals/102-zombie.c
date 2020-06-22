5f4631cdf3e3477ea46d7ab3dd8b26f1afe25cb3
luismvargasg <1610@holbertonschool.com>
2020-06-22 23:34:46 +0000

    removing wait

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -3,6 +3,21 @@
 #include <stdio.h>
 
 /**
+ * infinite_while - Funtion to wait infinite t until someone wants to kill Z.
+ *
+ * Return: Always 0.
+ */
+
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
+
+/**
  * main - Program that creates 5 zombie processes.
  *
  * Return: Nothing.
@@ -16,28 +31,10 @@ int main(void)
 	{
 		pid = fork();
 		if (pid > 0)
-			wait(50);
+			printf("Zombie process created, PID: %d\n", pid);
 		else
-		{
-			printf("Zombie process created, PID: %d\n", getpid());
 			exit(0);
-		}
 	}
 	infinite_while();
-	return (0);
-}
-
-/**
- * infinite_while - Funtion to wait infinite t until someone wants to kill Z.
- *
- * Return: Always 0.
- */
-
-int infinite_while(void)
-{
-	while (1)
-	{
-		sleep(1);
-	}
 	return (0);
 }
