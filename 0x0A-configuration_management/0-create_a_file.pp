6dc06b97f071fa9f23a6fbc899ee53bdc67afe06
luismvargasg <1610@holbertonschool.com>
2020-08-03 21:50:32 +0000

    adding task 0

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Puppet to create a file
+file { '/tmp/holberton':
+    ensure  => present,
+    path    => '/tmp/holberton',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I love Puppet',
+}
