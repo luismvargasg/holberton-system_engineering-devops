04e81af6e3825e3590552faea77453ca5cefe7aa
luismvargasg <1610@holbertonschool.com>
2020-08-04 18:25:08 +0000

    adding task 4

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,9 +1,9 @@
 # Puppet to make changes to our configuration file.
 ::ssh::client::config::user { 'ubuntu':
-  ensure  => present,
   options => {
-    'Host'                   => '104.196.209.226',
-    'IdentityFile'           => '~/.ssh/holberton',
-    'PasswordAuthentication' => 'no',
+    'Host 104.196.209.226' => {
+      'IdentityFile'           => '~/.ssh/holberton',
+      'PasswordAuthentication' => 'no',
+    }
   }
 }
