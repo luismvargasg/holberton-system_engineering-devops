dc7baa820eec8b0d94c3a9dae6fb241b29ac3cfb
luismvargasg <1610@holbertonschool.com>
2020-08-15 02:05:02 +0000

    adding task 2

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -1,7 +1,12 @@
 # Configures a brand new Ubuntu machine setting the custom HTTP header
 package { 'nginx':
+  name     => 'nginx',
   ensure   => 'installed',
   provider => 'apt',
+}
+exec { 'start nginx service':
+  path    => '/usr/sbin',
+  command => 'service nginx restart',
 }
 file_line { 'append a line in nginx config file':
   path  => '/etc/nginx/nginx.conf',
