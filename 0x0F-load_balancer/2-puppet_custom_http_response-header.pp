521c6130f0c64b3c279a57ce08d6c9b84f00c9b8
luismvargasg <1610@holbertonschool.com>
2020-08-15 02:06:40 +0000

    adding task 2

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -10,6 +10,6 @@ exec { 'start nginx service':
 }
 file_line { 'append a line in nginx config file':
   path  => '/etc/nginx/nginx.conf',
-  line  => 'add_header X-Served-By 1610-web-02;',
+  line  => 'add_header X-Served-By $HOSTNAME;',
   after => 'http {',
 }
