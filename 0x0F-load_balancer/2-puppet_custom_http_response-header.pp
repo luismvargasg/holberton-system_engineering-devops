95e77b56bea8a412802e06da7417a30d2d1850d6
luismvargasg <1610@holbertonschool.com>
2020-08-15 02:28:35 +0000

    adding hostname variable to call

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -10,7 +10,7 @@ package { 'nginx':
 }
 file_line { 'append a line in nginx config file':
   path  => '/etc/nginx/nginx.conf',
-  line  => "add_header X-Served-By ${hostname};",
+  line  => "\tadd_header X-Served-By ${hostname};",
   after => 'http {',
 }
 exec { 'sudo service nginx restart':
