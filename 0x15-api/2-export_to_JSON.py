7a43f1c1a7e92f4b16e1447988c765657b32f47c
luismvargasg <1610@holbertonschool.com>
2020-09-14 23:17:27 +0000

    reverting json dump x dumps

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -14,13 +14,13 @@ if __name__ == "__main__":
     r = requests.get("{}/todos".format(url))
     r_list = r.json()
     my_list = []
-    my_dict = {}
     json_dict = {}
     for task in r_list:
+        my_dict = {}
         my_dict['task'] = task.get('title')
         my_dict['completed'] = task.get('completed')
         my_dict['username'] = username
         my_list.append(my_dict)
     json_dict[id] = my_list
     with open('{}.json'.format(id), 'w') as file:
-        json.dumps(json_dict, file)
+        json.dump(json_dict, file)
