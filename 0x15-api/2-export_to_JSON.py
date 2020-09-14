d2cfa38c8d349c536b92b6056ff100c41b0d27fa
luismvargasg <1610@holbertonschool.com>
2020-09-14 23:11:39 +0000

    adding file for task 2

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Script that using a REST API, for a given employee ID, returns information
+about his/her TODO list progress inside a JSON file"""
+import json
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/users/{}".format(id)
+    r = requests.get(url)
+    username = r.json().get('username')
+    r = requests.get("{}/todos".format(url))
+    r_list = r.json()
+    my_list = []
+    my_dict = {}
+    json_dict = {}
+    for task in r_list:
+        my_dict['task'] = task.get('title')
+        my_dict['completed'] = task.get('completed')
+        my_dict['username'] = username
+        my_list.append(my_dict)
+    json_dict[id] = my_list
+    with open('{}.json'.format(id), 'w') as file:
+        json.dump(json_dict, file)
