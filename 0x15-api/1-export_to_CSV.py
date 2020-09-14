239d169bc6f737ff96ae662f99a75fccd260816a
luismvargasg <1610@holbertonschool.com>
2020-09-14 22:49:48 +0000

    adding missing information in the documentation

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Script that using a REST API, for a given employee ID, returns information
-about his/her TODO list progress."""
+about his/her TODO list progress inside a csv file"""
 import requests
 import sys
 
