75c870ce1b7fd5acc68a4d3ebd478e49e4fe5657
luismvargasg <1610@holbertonschool.com>
2020-09-15 00:46:35 +0000

    adding files for task 3

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -8,19 +8,20 @@ import requests
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/users"
     r = requests.get(url)
-    for user in range(1, len(r.json()) + 1):
-        res = requests.get("{}/{}".format(url, user))
-        username = res.json().get('username')
-        res = requests.get("{}/{}/todos".format(url, user))
-        r_list = res.json()
+    json_dict = {}
+    for user_id in range(1, len(r.json()) + 1):
+        json_dict[user_id] = []
+        user_res = requests.get("{}/{}".format(url, user_id))
+        username = user_res.json().get('username')
+        task_res = requests.get("{}/{}/todos".format(url, user_id))
+        task_res_list = task_res.json()
         my_list = []
-        json_dict = {}
-        for task in r_list:
+        for task in task_res_list:
             my_dict = {}
+            my_dict['username'] = username
             my_dict['task'] = task.get('title')
             my_dict['completed'] = task.get('completed')
-            my_dict['username'] = username
             my_list.append(my_dict)
-        json_dict[user] = my_list
-        with open('todo_all_employees.json', 'a') as file:
-            json.dump(json_dict, file)
+        json_dict[user_id] = my_list
+    with open('todo_all_employees.json', 'w') as file:
+        json.dump(json_dict, file)
