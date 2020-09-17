02ad9b42938971f1f1978c41fa836682f4b11776
luismvargasg <1610@holbertonschool.com>
2020-09-17 22:19:15 +0000

    adding files for task 1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Function that queries the Reddit API and prints the titles of the first 10
+hot posts listed for a given subreddit.
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """Function that returns the top ten posts"""
+    url = "https://www.reddit.com/r/{}/top.json".format(subreddit)
+    headers = {
+        'User-Agent':
+        'Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.2.3) \
+        Gecko/20100401 Firefox/3.6.3 (FM Scene 4.6.1)'}
+    r = requests.get(url, headers=headers, allow_redirects=False)
+    try:
+        response = r.json().get('data').get('children')
+    except:
+        print('None')
+        return
+    for item in range(10):
+        print(response[item].get('data').get('title'))
